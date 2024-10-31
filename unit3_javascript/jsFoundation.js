3fb5cacc3f13ebf564cbe712b3e846dcb4cc36d8
MarquiseSmalley <msmalley039@boyslatin.net>
2024-10-31 17:41:13 +0000

    Javascript - (var, let, const)

---
+++ unit3_javascript/jsFoundation.js
@@ -0,0 +1,36 @@
+// Data types= building blocks of the Javascript programming
+// programming language.
+
+// Strings = data bound together by quotation marks.
+"this is a sentence wrapped in quotes- this is a string"
+"1232321- this is a number BUT, when in quotes, it is string"
+
+// Integer = numbers- doesn't matter if it's a whole number
+// or a decimal
+88678
+789
+1342.785
+0
+
+// boolean = true or false
+true, 1
+false, 0
+
+// variables = are containers for data
+// when writing a variable, we break it down into 3 steps
+// 1. declare the type of variable we're working with
+// 2. we need to give the variables names
+// 3. we need an equal sign
+// 4. we need to give it a value/ data
+var a ='// universal container- cant go wrong using this.'
+let b ='// temporary variables- data that will change'
+const c ='// constant variables- data that will NOT change'
+
+let weather_by_city = 'philly'
+let weather_current_temp =80
+let weather_by_day = 'thursday'
+const logo= './weatherchannellogo.jpg'
+const saved_currentCity = 'Philly'
+var weather_10day = true
+
+print ('sending message to terminal.')
