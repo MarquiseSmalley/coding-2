f050bbccc41c991493bc9f80d69e46ab5a2813d8
MarquiseSmalley <msmalley039@boyslatin.net>
2024-11-01 17:40:14 +0000

    Nov 1st Async Work

---
+++ unit3_javascript/November 1st 2024 Async Classwork.js
@@ -0,0 +1,16 @@
+1. Describe the difference between var, let, and const. Your answer can be
+formatted as a comment or a string. 
+2. Use what you've learned about data types and variables and create three 
+variables that when multiplied together will result in the value 4000. 
+Research the console.log() function on w3schools to output your result in the terminal. 
+3. Describe a scenario where you would use the let variable and when you 
+would use a const variable in a computer program. Your answer can be
+formatted as a comment or a string. 
+4. If there were three things you that feel could improve the class, what would those things be and why?
+Your answer can be formatted as a comment or a string. 
+
+My Answers: 
+'1. The difference between var, let, and const is that let has data will change, while const has data that wont change.'
+'2. console (2 x 2 x 1000 = 4000)'
+'3. A scenario where the let variable would be used is the date on your device. A scenario where the const variable would be used is the calculator apps format.' 
+'4. Three things that could improve the class are group learning (class sepearated into multiple groups of different subjects), possible prizes for projects, and just a higher class work ethic.
