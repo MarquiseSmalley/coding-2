a161d3810c7d6acd70191eefcdc891f1b7275bab
MarquiseSmalley <msmalley039@boyslatin.net>
2024-11-07 18:40:45 +0000

    jsPopQuiz_6thPeriod.js

--- unit3_javascript/November1st2024AsyncClasswork.js
+++ unit3_javascript/November1st2024AsyncClasswork.js
@@ -13,4 +13,4 @@ My Answers:
 '1. The difference between var, let, and const is that let has data will change, while const has data that wont change.'
 '2. console (2 x 2 x 1000 = 4000)'
 '3. A scenario where the let variable would be used is the date on your device. A scenario where the const variable would be used is the calculator apps format.' 
-'4. Three things that could improve the class are group learning (class sepearated into multiple groups of different subjects), possible prizes for projects, and just a higher class work ethic.
+'4. Three things that could improve the class are group learning (class sepearated into multiple groups of different subjects), possible prizes for projects, and just a higher class work ethic.)
---
+++ unit3_javascript/jsPopQuiz_6thPeriod.js
@@ -0,0 +1,60 @@
+// JavaScript Pop Quiz
+// Create a new document called jsPopQuiz.js and answer the following questions.
+// When you have completed the quiz, please commit and sync your work so that it can be
+// graded.
+
+// RULES
+// You are ONLY permitted to use W3 schools. 
+// You may NOT use any other external websites.
+// No music is permitted during the quiz. 
+// All phones must be put away- If a phone is visible, you will recieve a verbal warning.
+// A second warning will result in an automatic failure. 
+// Once you've completed your quiz, you may use the computer at your leisure- PLEASE DO NOT
+// disrupt/ distract your classmates who are still taking the quiz. 
+ 
+// 1. How do you declare a variable in JavaScript? Please write your answer 
+// using JavaScript code. 
+
+// Automatically 
+// Using var
+// Using let
+// Using const
+
+// 2. How would you declare a variable where the data WILL NOT change in JavaScript?
+// Please write your answer using JavaScript code. 
+
+// The const variable
+
+// 3. Identify the following JavaScript operators and describe when you would use it?
+// Please write your answers using the string data type. 
+//  var = a ='// universal container- cant go wrong using this.'
+//  let = b ='// temporary variables- data that will change'
+//  const = c ='// constant variables- data that will NOT change'
+
+// 4. Describe a scenario where you would use a comparison operator,
+// and then provide a code example based on the scenerio you provided. 
+
+// A scenario where the comparison operator would be useful is comparing two or more variables together.
+
+// 5. Describe a scenario where you would use a logical operator,
+// and then provide a code example based on the scenerio you provided. 
+
+// A scenario where the logical operator would be useful is contrasting two or more variables against one another.
+
+// 6. Create three variables where when combined will result in the value of 36. 
+// Please use the console.log () to out put your result. 
+
+console.log( 2 * 6 * 3)
+
+// 7. Fill in the variable that will output the appropriate result. 
+
+var x ='Please replace this text with your answer.'
+var y = x *2
+
+// Your console log should output the value 28.
+console.log(y)
+
+
+// 8. What does it "concatenation" mean? Please write your response as a string
+// and also provide a code example of two variables being concatenated and output the result
+// using the console.log() function. 
