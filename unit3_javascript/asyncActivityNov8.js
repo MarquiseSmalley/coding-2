b50ef6f714ef31bd66353d861ebe7388f844b199
MarquiseSmalley <msmalley039@boyslatin.net>
2024-11-12 18:38:23 +0000

    async

---
+++ unit3_javascript/asyncActivityNov8.js
@@ -0,0 +1,60 @@
+// 1. What does it "string concatenation" mean? Please write your response as a string and also provide a code example of two variables being concatenated and output the result in your terminal.
+// if you are learning JavaScript you will be using the console.log() function.
+
+// if you are learning Pyhon you will be using the print() function.
+
+// 2. Please list and describe three rules for creating variable names. Please provide your answer as a string.
+// 'var - universal/ default variable, can be used in any context'
+// 'let - is used to declare variables that WILL change'
+// 'const - is used to declare variables that will NOT change'
+
+// 3. Define each of the following variable name formats then provide a code example of how you would write them.
+
+// Camel case - every word after the first word is capitalized
+// var userName = ''
+// var timeRemainingInQrtr= ''
+
+// Snake case - every word is seperatd by underscores.
+// var userName = ''
+// var time_Remaining_In_Qrtr= ''
+
+// Pascal case -
+// var User_Name =''
+// var
+
+//4. Find the missing variable for each of the following assignment operaor code snippets
+Python Version a = '?'
+
+b = a *100
+
+print(b) // the output in the terminal should be 300.
+
+// JavaScript Version 
+let a = '350'
+
+let b = a *2
+
+console.log(b) // the output in the terminal should be 700.
+
+// 5. Use the logical operators and evaluate the following variables to get the requested result.
+// Python Version
+
+which logical operator would you use to reflect that the result between the code expressions are true. use the print function to output the result in the termial, then explain why you would use that particular logical operator using a string.
+x= 10
+
+y= 2
+
+z= y*2
+
+console.log (x > 1 and z==4)
+
+JavaScript Version
+
+which logical operator would you use to reflect that the result between the code expressions below are false. Use the console.log() function to output the result in the termial, then explain why you would use that particular logical operator using a string.
+var x= 12
+
+var y= 2
+
+var z= 12/4
+
+x <= 12 _____ z < 10
