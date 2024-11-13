e08d882d5095a1c77020ad3d6ee01e66ab17c13f
MarquiseSmalley <msmalley039@boyslatin.net>
2024-11-13 18:41:49 +0000

    Arithmetic Operators

---
+++ unit3_javascript/operatorReview.js
@@ -0,0 +1,33 @@
+// Arithmetic Operators - processes that involve math.
+let
+let 
+
+function withdrawMusicSub(){
+    //if money exists - withdraw money for sub
+    console.log(bankAccount-musicSubscriptionPlan_1)
+    // if no money - pause sub
+}
+
+// Assignment Operators - 
+// MAIN TAKEAWAYS : the single equal symbol/sign is used for assignment
+
+var jeans = 200.0
+jeans *= 0.25;
+var discount = (jeans *= 25);
+var calculatedDiscount = jeans * discount
+var total = jeans - calculatedDsicount
+
+console.log(total);
+
+// Comparison Operators - analyzing values to one another
+// the equal symbol has special properties in JS.
+// single equal (=) : ONLY FOR ASSIGNMENT
+var number = 4
+// double equal (==) : same as or equal to
+var a = 10
+var b = 12
+console.log(a==b)
+// triple equal : equal value and equal type
+var depositAmount = 400.00
+var savingsAccount = 400.00
+// exclamation equal (!=) : not equal
