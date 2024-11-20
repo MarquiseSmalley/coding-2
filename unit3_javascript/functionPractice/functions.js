e9bf69715d84e6aae42e3daa83e4a28294eb97dc
MarquiseSmalley <msmalley039@boyslatin.net>
2024-11-20 18:39:00 +0000

    Depositted and Withdrawed Money

---
+++ unit3_javascript/functionPractice/functions.js
@@ -0,0 +1,37 @@
+// What is a function?
+'A function is a set of instructions that perform a specific task'
+'or job'
+
+// There are two variations of functions:
+// Built-in functions: these are function instructions that 
+// are already in the programming language and all you need to do 
+// is pass it data.
+
+// console.log() is an example of a built-in function
+name = 'marquise smalley'
+console.log(name)
+
+// User defined functions: these are custom instuctions written
+// engineers to make code that performs specific tasks.
+
+// How do we write a user-defined function?
+function exampleFunction(){
+    console.log('The function script is connected')
+}
+
+// in order to have a function work, we need to call it.
+// the above code is the function definition - MEANING - it's
+// telling the computer the instructions- NOT to actually run
+
+// we access or call a function by writing its name.
+//exampleFunction()
+let accountBalance = 100
+
+const taxes =.07
+
+function depositMoney(x){
+    console. log('money has been added succesfully.')
+    console.log(accountBalance + x)
+}
+
+
