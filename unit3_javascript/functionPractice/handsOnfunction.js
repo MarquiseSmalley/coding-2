437141659bf1fdbf96811b92bc3db5fe2e2257d3
MarquiseSmalley <msmalley039@boyslatin.net>
2024-12-04 18:40:25 +0000

    Else if

--- unit3_javascript/functionPractice/handsOnfunction.js
+++ unit3_javascript/functionPractice/handsOnfunction.js
@@ -33,6 +33,33 @@ function checkTVInventory(tvs) {
 
 checkTVInventory(30)
 
+// Activity 
+
+// 1. Create a function that will take a number that 
+// is passed into the functions parameters and convert 
+// the number into minutes. For example, a value of 2 
+// should return 120 minutes. a value of 3 should 
+// return 180 minutes, etc.
+
+function hoursToMinutes(hour){
+    if (hour == 12 ){
+      console.log("You will get normal pay for the day.");
+    } else if (hour > 8) {
+      console.log("You qualify for overtime.");
+    } else if (hour > 16) {
+      console.log("You're working way too hard.");
+    } else if (hour >= 24)
+    }
 
 
 
+doctorPay(25);
+
+
+
+function discount(itemPrice){
+    console.log('you qualify for 5% discount')
+}
+else if(itemPrice)
+}
+// 
