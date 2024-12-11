d8df59adc699ec07be65dff3784a6085276adf59
MarquiseSmalley <msmalley039@boyslatin.net>
2024-12-11 20:39:54 +0000

    GradeScaleAssignmentDec112024

--- unit3_javascript/functionPractice/jsConditionalReview.js
+++ unit3_javascript/functionPractice/jsConditionalReview.js
@@ -9,16 +9,17 @@
 // we recieve
 
 function timeOfDay(time) {
-if  (time == 'am')
+  if (time == 'am') {
     console.log("it is the morning.");
   } else if (time == 'pm') {
-    console.log ("it is the evening.");
+    console.log("it is the evening.");
   } else {
     console.log("sorry, cannot understand input.");
+  }
+
 }
+//timeOfDay()
 
-
-timeOfDay()
 // create a function that will give 
 // someone a letter grade based on their
 // numerical grade score. For example,
@@ -29,12 +30,31 @@ timeOfDay()
 // etc.
 
 function GradeScale(grade) {
-    if (grade == '90')
-        console.log("the grade is an A.");
-      } else if (grade == '80') {
-        console.log ("the grade is a B.");
-      } else if (grade == '70')
-        console.log("the grade is a C.")
-      { else if (grade == '60') 
-      
-    
+  if (grade > 90) {
+    console.log("the grade is an A.");
+  } else if (grade > 80 && grade < 90) {
+    console.log("the grade is a B.");
+  } else if (grade > 70 && grade < 80) {
+    console.log("the grade is a C.")
+  } else if (grade > 60 && grade < 70) {
+    console.log("the grade is a D.")
+  } else {
+    console.log('you did not pass try again')
+
+  }
+}
+
+
+GradeScale(86)
+//nested condtions are functions inside
+// of functions. this gives us more power
+// to make unique outcomes\.
+//   function passwordRetrieval(email, password, sQuestion)
+//   if(email == dbEmails.sort()) {
+//       console.log("correct")
+//   console.log("please provide a password?")
+// }else{
+//   console.log('sorry, this email doesnt exist')
+// }
+
+
