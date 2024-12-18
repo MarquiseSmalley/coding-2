5c9254ad80c683ee58987384147e79e08dca55ec
MarquiseSmalley <msmalley039@boyslatin.net>
2024-12-18 18:37:48 +0000

    DiscountFunctionActivity

--- unit3_javascript/functionPractice/Dec132024AsyncActivity.js
+++ unit3_javascript/functionPractice/Dec132024AsyncActivity.js
@@ -5,10 +5,15 @@
 
 function NumberLabeler (number) {
     if (number == -1) {
+
       console.log("this is a negative number.");
+
     } else if (number == 1) {
+
       console.log("this is a positive number.");
+
     } else if (number == 0){
+
       console.log('this is zero.')
   
     }
@@ -44,12 +49,67 @@ function TicketAge (age) {
 
 
 // 3. You have been hired by target to assist them with their store member discount software. 
-// The would like to make it so that shoppers who have a specific membership tier can save a certain amount of money on the products they buy.
+// They would like to make it so that shoppers who have a specific membership tier can save a certain amount of money on the products they buy.
 // Provided below are the memberships and the discount amount they should recieve:
 // superShopper should recieve a 10% discount on their items
 // megaShopper should recieve a 15% discount on their items
 // ultraShopper should receive a 20% discount on their items
 
-// Your program should be able to take in the shoppers membership type, the name of the item they are purchasing, and the item price, and should return a message telling the user what the final price of the item is and how much they saved.
+// Your program should be able to take in the shoppers membership type, 
+// the name of the item they are purchasing, and the item price, 
+// and should return a message telling the user 
+// what the final price of the item is and how much they saved.
 
 // For example: congratulations superShopper, you saved $10.00 on this TV. Your final item price is $90.00.
+
+function MembershipDiscount (Discount) {
+  if (membership == superShopper) {
+    console.log("congratulations superShopper, you saved $10.00 on this TV. Your final item price is $90.00.");
+  } 
+  else if (membership == megaShopper) {
+    console.log("congratulations megaShopper, you saved $15.00 on this TV. Your final item price is 85.00.");
+  } 
+  else if (membership == ultraShopper) {
+    console.log("congratulations ultraShopper, you saved $20.00 on this TV. Your final item price is $80.00.");
+  } 
+
+  }
+
+  MembershipDiscount(ultraShopper)
+
+  // # KEY POINTS
+  // # Determine the price of the item baseed on the user's membership.
+  // # - superShopper should recieve a 10% discount on their items
+  // # - megaShopper should recieve a 15% discount on their items
+  // # - ultraShopper should receive a 20% discount on their items
+  // # we need an item name, item price, membership, and message.
+
+  function discountFunction(membership, itemPrice) {
+    console.log("Testing. Function works.");
+    if(membership === "superShopper") {
+      console.log("you are a super shopper, here is a 10% discount on your item.");
+      var discountAmount = itemPrice * 0.1
+      console.log(discountAmmount);
+      var total = itemPrice - discountAmmount;
+      console.log(total);
+
+    } else if(membership === "megaShopper") {
+        console.log("you are a mega shopper, here is a 15% discount on your item.");
+        var discountAmount = itemPrice * 0.15;
+        console.log(discountAmmount);
+        var total = itemPrice - discountAmmount;
+        console.log(total);
+  
+    } else if(membership === "ultraShopper") {
+        console.log("you are an ultra shopper, here is a 20% discount on your item.");
+        var discountAmount = itemPrice * 0.20
+        console.log(discountAmmount);
+        var total = itemPrice - discountAmmount;
+        console.log(total);
+  
+      } else {
+      console.log("Error, sorry this membership doesnt exist.");
+     }
+    
+
+discountFunction ('superShopper', 100);
