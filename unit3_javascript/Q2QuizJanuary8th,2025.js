943bc2f9f3c4d4c9485fd2f8e4978433e1bbe9e6
MarquiseSmalley <msmalley039@boyslatin.net>
2025-01-08 18:40:51 +0000

    Q2QuizJanuary8th,2025

---
+++ unit3_javascript/Q2QuizJanuary8th,2025.js
@@ -0,0 +1,19 @@
+// 1. An integer data type is a data type that deals with numerical sets or ranges.
+
+// 2. An if/else conditional statement is a statement which seperates levels or ranks into categories.
+
+// 3. A function is a block of code that instructs a machine to perform an action.
+
+// 4. A boolean data type is a data type that differs between true and false.
+
+// 5. The difference between a function parameter and a function argument is that a function parameter is more of the title given to a function while a function argument is more of what trhe function comes to mean.
+
+// 6. The best working operator for a digital locker function would be an integer data type.
+
+// 7. The best working operator for a height verification function would be the if/else format.
+
+// 8. 300,000 * .07 = 21,000
+
+// 9. 
+
+// 10. 
