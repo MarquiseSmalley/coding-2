2bd46a4d897bf312a4a5d22070b31abd23a51ab8
MarquiseSmalley <msmalley039@boyslatin.net>
2025-01-10 18:44:03 +0000

    january10,2025ASYNCACTIVITY.JS

---
+++ unit3_javascript/functionPractice/javaScriptLoops.js
@@ -0,0 +1,20 @@
+// A list is a unique grouping data type that allows
+// use to collect and organize data in one variable.
+
+// lists can contain viritually all data types
+// simultaneously.
+
+var GroceryList= ['Name', 10, true]
+
+var student1 = ["will", 90, "football"];
+var student2 = ["jaydin", 92, "basketball"];
+var student3 = ["marquise", 80];
+var codingClass6Period = ['will','jaydin','haleem','etc...']
+
+console.log(codingClass6Period);
+
+var pegasus
+
+// find the item in the list at index 3, and 1.
+// You can use w3schools, Freecodecamp, MDN docs.
+var groceryList = ['apple','water','milk','paper towels','dish soap']
